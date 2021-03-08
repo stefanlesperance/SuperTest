1e2764cde74a7594f0b75647d04639163cad15db
Stefan Lesperance <slesperance@conamex.com>
2021-03-08 04:22:36 -0500

    First Posting + Associated Files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :posts
   get 'about' => "pages#about"
-  get 'pages/test'
+  get 'test' => "pages#test"
 
   root 'pages#welcome'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
