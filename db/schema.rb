4bc17b2e66d5f392e61aaaf25bc2ef84d5ed2c51
Stefan Lesperance <slesperance@conamex.com>
2021-03-08 14:53:11 -0500

    Working FOrward

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_090912) do
+ActiveRecord::Schema.define(version: 2021_03_08_173959) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "kind"
   end
 
 end
