f2743fe5c6d1ab8847ec96924fd16841ce111234
Stefan Lesperance <slesperance@conamex.com>
2021-03-08 06:09:41 -0500

    Posting Section and Styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -43,9 +43,7 @@ class PostsController < ApplicationController
   # DELETE /posts/1 or /posts/1.json
   def destroy
     @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
-    end
+    redirect_to posts_url
   end
 
   private
